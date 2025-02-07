0d431ea5435965d27936805072b774039501ff92
BEERESHKUMAR B C <148090219+beereshbc@users.noreply.github.com>
2025-02-07 17:49:03 +0530

    Update Services.jsx

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -28,54 +28,6 @@ const Services = () => {
 
   return (
     <div>
-
-
-{/* <div className=' flex gap-2'>
-<select
-           className="py-1 px-3 border rounded text-sm transition-all"
-          onChange={(e) => setCityData(e.target.value)}
-          value={'Davanagere'}
-          id="city-select"
-        >
-          <option value="Bagalkot">Bagalkot</option>
-          <option value="Bengaluru Urban">Bengaluru Urban</option>
-          <option value="Bengaluru Rural">Bengaluru Rural</option>
-          <option value="Belagavi">Belagavi</option>
-          <option value="Ballari">Ballari</option>
-          <option value="Bidar">Bidar</option>
-          <option value="Vijayapur">Vijayapur</option>
-          <option value="Chamarajanagar">Chamarajanagar</option>
-          <option value="Chikballapur">Chikballapur</option>
-          <option value="Chikkamagaluru">Chikkamagaluru</option>
-          <option value="Chitradurga">Chitradurga</option>
-          <option value="Dakshina Kannada">Dakshina Kannada</option>
-          <option value="Davanagere">Davanagere</option>
-          <option value="Dharwad">Dharwad</option>
-          <option value="Gadag">Gadag</option>
-          <option value="Kalaburagi">Kalaburagi</option>
-          <option value="Hassan">Hassan</option>
-          <option value="Haveri">Haveri</option>
-          <option value="Kodagu">Kodagu</option>
-          <option value="Kolar">Kolar</option>
-          <option value="Koppal">Koppal</option>
-          <option value="Mandya">Mandya</option>
-          <option value="Mysuru">Mysuru</option>
-          <option value="Raichur">Raichur</option>
-          <option value="Ramanagara">Ramanagara</option>
-          <option value="Shivamogga">Shivamogga</option>
-          <option value="Tumakuru">Tumakuru</option>
-          <option value="Udupi">Udupi</option>
-          <option value="Uttara Kannada">Uttara Kannada</option>
-          <option value="Yadgir">Yadgir</option>
-          <option value="Vijayanagara">Vijayanagara</option>
-        </select>
-
-        <button onClick={''} className="py-1 px-3 border rounded text-sm transition-all">Search</button>
-</div> */}
-
-
-
-
       <p className='text-gray-600'>Browse through the Service specialists</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
         <button onClick={() => setShowFilter(prev => !prev)} className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ""}`}>Filters</button>
